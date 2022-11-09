db4cb70a9bcc2ddfc1449862292581c9fc103e5e
Luis Arcos <1813110043@untels.edu.pe>
2022-11-09 09:23:03 -0500

    first commit

---
+++ aplicacion.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+menu = int(input("Menu Opciones:\n 1- Registrar\n 2. Eliminar\n 3. Editar\n 4. Listar\n 5. Salir\n"))
+while menu !=0:
+    if menu == 1:
+        print ("Registrar")
+    elif menu == 2:
+        print ("Eliminar")
+    elif menu == 3:
+        print ("Editar")
+    elif menu == 4:
+        print ("Listar")
+    elif menu == 5:
+        print ("Salir")
+    else:
+        print ("elegir una opcioón correcta")
+    
+    menu = int(input("\nMenu Opciones:\n 1- Registrar\n 2. Eliminar\n 3. Editar\n 4. Listar\n 5. Salir\n"))
+        
+    
